9cdcebdad167422daa7d31dac3ca98f5103aa96f
matanhayon <matanhayon@gmail.com>
2023-08-19 16:10:39 +0300

    gdfg

---
+++ FacebookWinFormsApp/PostsLogic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BasicFacebookFeatures
+{
+    public static class PostsLogic
+    {
+        private static FormMain m_Form;
+
+        public static PostsLogic(FormMain i_Form)
+        {
+            m_Form = i_Form;
+        }
+
+    }
+}
